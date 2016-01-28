42f61cee222103fd4fffd8d8fefc5db0b7bc25f3
cfs workload <cfsworkload@bg.vnet.ibm.com>
2016-01-28 23:39:05 +0000

    changes

--- front-end/app.js
+++ front-end/app.js
@@ -67,9 +67,13 @@ app.use(function(err, req, res, next) {
 	});
 });
 
-res.header('Access-Control-Allow-Origin', 'http://localhost:8080/');
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        next();
+});
 
 var appClientConfig = {
 	"org": services['iotf-service'][0]['credentials'].org,
