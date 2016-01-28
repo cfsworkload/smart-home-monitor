9128f44fa9195cfbdb7481a07253695431463785
Nathan Chaney <nrchaney@us.ibm.com>
2016-01-28 20:20:23 +0000

    preliminary front-end

--- front-end/public/javascripts/scripts.js
+++ front-end/public/javascripts/scripts.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
-  var socket = io.connect('http://localhost:3000');
+  window.location.origin = window.location.origin || (window.location.protocol + '//' + window.location.hostname + ':' + window.location.port);
+  var socket = io.connect(window.location.origin);
   socket.on('update-msg', function (msg) {
-    console.log(msg);
-    $('#mydiv').html(msg.data)
+    //console.log(msg);
+    var data = jQuery.parseJSON(msg.data);
+    console.log(data);
+    $('.output').css('visibility', 'visible');
+    $('#temperature').html(data.temp + '\u2103');
+    $('#humidity').html(data.humidity + '%');
+    $('#deviceid').html(data.DeviceID);
+
+    var latitude = data.LocLat;
+    if(latitude < 0) latitude = (-1 * latitude) + '\u00B0S';
+    else latitude = latitude + '\u00B0N';
+    $('#loclat').html(latitude);
+
+    var longitude = data.LocLong;
+    if(longitude < 0) longitude = (-1 * longitude) + '\u00B0W';
+    else longitude = longitude + '\u00B0E';
+    $('#loclong').html(longitude);
+
+    $('#policyid').html(data.PolicyID);
   });
 });
