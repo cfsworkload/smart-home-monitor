9bd8c2ca080b2099b0615b1f945ef7c610db20d9
cfs workload <cfsworkload@bg.vnet.ibm.com>
2016-01-29 15:23:21 +0000

    updates

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  var mqtt = require('mqtt');
+  var mqttClient = mqtt.connect('mqtt://test.mosquitto.org');
+  mqttClient.on('connect', function () {
+  mqttClient.publish('wesley-test-999', '{ "temp" : 25, "humidity" : 77, "DeviceID" :' + process.env.VCAP_APP_HOST + ', "LocLat" : 37.3382, "LocLong" : -121.8863, "PolicyID" : 6134141 }');
+  });
 });
 
 module.exports = router;
