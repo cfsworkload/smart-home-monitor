e95defa4d8c77613278e70c7787cc434903e83d2
cfs workload <cfsworkload@bg.vnet.ibm.com>
2016-02-01 18:45:39 +0000

    adding of callback

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, callback) {
   
   res.render('index', { title: 'Express' });
   var mqtt = require('mqtt');
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   mqttClient.on('connect', function () {
   mqttClient.publish('wesley-test-999', '{ "temp" : 99, "humidity" : 99, "DeviceID" : "insurance-01", "LocLat" : 37.3382, "LocLong" : -121.8863, "PolicyID" : 6134141 }', function () { console.log("We returned from callback"); });
   });
-
+  callback;
 
 });
 
